8d97b7b408325c546ad16de01aa3ee13f9e62f97
dminter <xm0ff255@126.com>
2017-05-12 18:41:54 +0800

    bug fix

--- app/src/main/java/com/zncm/jmxandroid/utils/Xutils.java
+++ app/src/main/java/com/zncm/jmxandroid/utils/Xutils.java
@@ -1,6 +1,9 @@
 package com.zncm.jmxandroid.utils;
 
+import android.content.Context;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.zncm.jmxandroid.base.MyApp;
@@ -39,6 +42,55 @@ public class Xutils {
         }
         Toast.makeText(MyApp.getInstance().ctx, msg, Toast.LENGTH_LONG).show();
     }
+
+
+
+
+
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public static int dip2px(float dpValue) {
+        final float scale = MyApp.getInstance().ctx.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int px2dip(float pxValue) {
+        final float scale = MyApp.getInstance().ctx.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+
+    public static final WindowManager wm = (WindowManager) MyApp.getInstance().ctx.getSystemService(Context.WINDOW_SERVICE);
+
+    public static DisplayMetrics getDeviceMetrics() {
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+        return dm;
+    }
+
+    public static int getDeviceHeight() {
+        return getDeviceMetrics().heightPixels;
+    }
+
+    public static int getDeviceWidth() {
+        return getDeviceMetrics().widthPixels;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- app/src/main/java/com/zncm/jmxandroid/view/AZView.java
+++ app/src/main/java/com/zncm/jmxandroid/view/AZView.java
@@ -5,17 +5,22 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.zncm.jmxandroid.utils.Xutils;
+
+import java.util.ArrayList;
 
 /**
  * Created by jiaomx on 2017/5/12.
  */
 
 public class AZView extends View {
+    ArrayList<String> letters = new ArrayList<>();
     private Paint paint;
-
+    int deviceWidth = Xutils.getDeviceWidth();
+    int deviceHeight = Xutils.getDeviceHeight();
     public AZView(Context context) {
         super(context);
         init();
@@ -24,6 +29,11 @@ public class AZView extends View {
     private void init() {
         paint = new Paint();
         paint.setStrokeWidth(5);
+        paint.setTextSize(Xutils.dip2px(16));
+        for (int i = 0; i <26 ; i++) {
+            char letter = (char) ('A'+i);
+            letters.add(String.valueOf(letter));
+        }
     }
 
     public AZView(Context context, @Nullable AttributeSet attrs) {
@@ -40,11 +50,37 @@ public class AZView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        for (int i = 0; i <26 ; i++) {
-            char letter = (char) ('a'+i);
-            Xutils.debug("letter::"+letter);
-            canvas.drawText(letter+"",100,100,paint);
+        Xutils.debug("onDraw------onDraw");
+
+
+
+        for (int i = 0; i <letters.size() ; i++) {
+            char letter = (char) ('A'+i);
+            canvas.drawText(letter+"",deviceWidth-Xutils.dip2px(20),Xutils.dip2px(20)+deviceHeight*i/30,paint);
+            Xutils.debug("letter::"+letter+" "+(Xutils.dip2px(20)+deviceHeight*i/30));
         }
+
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()){
+
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float fingerY = event.getY();
+                int pos = (int) (Xutils.dip2px(20)-fingerY/(deviceHeight/letters.size()));
+                Xutils.debug("fingerY::"+fingerY+" "+letters.get(pos));
+                break;
+
+        }
+
+        return true;
+
+
 
 
     }
